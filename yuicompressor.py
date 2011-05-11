a6f32b20066670c79ccbebf53a842b80f8f46b33
Ian Halpern <ian@ian-halpern.com>
2011-05-11 01:48:53 -0400

    * added support for cli options

--- yuicompressor.py
+++ yuicompressor.py
@@ -1,5 +1,20 @@
 import subprocess
 
-def compress( jsfile ):
-	cmd = 'yui-compressor %s' % jsfile
+def compress( jsfile, **options ):
+	"""
+		To add cli options pass the option as an argument.
+
+		Example:
+			compress( 'file.js', nomunge=True, line_break=4 )
+	"""
+	options = ' '.join(
+		map( lambda k: ( '--' if len(k) > 1 else '-' ) + k.replace( '_', '-' ) + ( ' %s' % options[k] if options[k] != True else '' ),
+		[ k for k in options.keys() if options[k] ] )
+	)
+
+	cmd = 'yui-compressor %s %s' % ( options, jsfile )
+
 	return subprocess.Popen( cmd, shell=True, stdout=subprocess.PIPE ).stdout.read()
+
+if __name__ == '__main__':
+	print compress( '', h=True )
